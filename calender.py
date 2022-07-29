e912cdfc828b8c81cfaca206157ccf245b6794b8
n-mbachia <ndmbachia@outlook.com>
2022-07-29 20:46:38 +0300

    Progress bar

---
+++ calender.py
@@ -0,0 +1,9 @@
+from calendar import*
+year = int(input('Enter Year: '))
+print(calendar(year, 2, 1, 8, 4))
+"""
+2 = 2 character for days (Mo, Tu etc)
+1 = 1 row for each week
+8 = 8 rows foe each month
+4 = 4 column for all months of the year
+"""
